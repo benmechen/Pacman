c9e6a52c0df6da3d6d27f442b233c67c3d54490a
Ben <benm123@yahoo.com>
2017-03-03 22:21:53 +0000

    Add PATMAN files

---
+++ src/PATMAN.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      11mechenb
+#
+# Created:     03/03/2017
+# Copyright:   (c) 11mechenb 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import pygame
+
+class Player:
+    def __init__(self):
+        self.rect = pygame.Rect(10, 10, 10, 10)
+
+    def move(self, dy, dx):
+        if
+
+pat = Player()
