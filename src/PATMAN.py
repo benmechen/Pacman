ed19ba6f65c9a16a8068a9eb6c9cf503ab37f727
Ben <benm123@yahoo.com>
2017-03-11 10:50:56 +0000

    Add ghosts AI, add different coloured ghosts

--- src/PATMAN.py
+++ src/PATMAN.py
@@ -2,11 +2,13 @@
 # @Author: Ben
 # @Date:   2017-03-03 22:23:44
 # @Last Modified by:   Ben
-# @Last Modified time: 2017-03-08 20:29:19
+# @Last Modified time: 2017-03-11 10:49:28
 import pygame
 import time
 import random
 import sys
+from collections import Counter
+from math import sqrt
 
 class Game:
     def __init__(self):
@@ -18,9 +20,9 @@ class Game:
         for ghost in ghosts:
             if player.rect.x == ghost.rect.x and player.rect.y == ghost.rect.y:
                 return False
-            elif player.rect.x+1 == ghost.rect.x and player.rect.y+1 == ghost.rect.y:
+            elif player.rect.x+10 == ghost.rect.x and player.rect.y+10 == ghost.rect.y:
                 return False
-            elif player.rect.x-1 == ghost.rect.x and player.rect.y-1 == ghost.rect.y:
+            elif player.rect.x-10 == ghost.rect.x and player.rect.y-10 == ghost.rect.y:
                 return False
         return True
 
@@ -83,34 +85,20 @@ class Ghost:
         ghosts.append(self)
         self.rect = pygame.Rect(pos[0], pos[1], 20, 20)
         self.direction = 3
+        self.nextdirection = 3
         self.count = 0
 
-    def move(self, dy, dx, teleportcoords):
+    def move(self, dy, dx):
         if dx != 0:
-            self.move_single_axis(dx, 0)
+            if self.move_single_axis(dx, 0) == False:
+                return False
+            else:
+                return True
         if dy != 0:
-            self.move_single_axis(0, dy)
-
-        try:
-            y = int(self.rect.y/10)
-        except:
-            y = int(self.rect.y)
-
-        try:
-            x = int(self.rect.x/10)
-        except:
-            x = int(self.rect.x)
-
-        if [x, y] in teleportcoords:
-            if teleportcoords.index([x, y]) == 0:
-                self.rect.x = (teleportcoords[1][0])*10
-                self.rect.y = (teleportcoords[1][1])*10
-                self.direction = 2
-            elif teleportcoords.index([x, y]) == 1:
-                self.rect.x = (teleportcoords[0][0])*10
-                self.rect.y = (teleportcoords[0][1])*10
-                self.direction = 3
-
+            if self.move_single_axis(0, dy) == False:
+                return False
+            else:
+                return True
 
     def move_single_axis(self, dx, dy):
         self.rect.x += dx
@@ -126,10 +114,42 @@ class Ghost:
                     self.rect.bottom = wall.rect.top
                 if dy < 0:
                     self.rect.top = wall.rect.bottom
-                self.direction = random.randrange(0, 4)
-        self.count += 1
-        if self.count % random.randrange(2, 10) == 0:
-            self.direction = random.randrange(0, 4)
+                return False
+        return True
+
+    def chase(self, player, collision):
+        print(collision)
+        x1 = player.rect.x
+        y1 = player.rect.y
+
+        up = down = left = right = 1000000
+
+        #Up
+        if "0" not in collision:
+            x2 = self.rect.x
+            y2 = self.rect.y-10
+            up = sqrt(((x2-x1)**2)+((y2-y1)**2))
+
+        #Down
+        if "1" not in collision:
+            x2 = self.rect.x
+            y2 = self.rect.y+10
+            down = sqrt(((x2-x1)**2)+((y2-y1)**2))
+
+        #Left
+        if "2" not in collision:
+            x2 = self.rect.x-10
+            y2 = self.rect.y
+            left = sqrt(((x2-x1)**2)+((y2-y1)**2))
+
+        #Right
+        if "3" not in collision:
+            x2 = self.rect.x+10
+            y2 = self.rect.y
+            right = sqrt(((x2-x1)**2)+((y2-y1)**2))
+
+        distances = [up, down, left, right]
+        return distances.index(min(distances))
 
 pygame.init()
 pygame.display.set_caption("PATMAN")
@@ -181,15 +201,22 @@ patman_open = pygame.image.load('patman_open.png')
 patman_open = pygame.transform.scale(patman_open, (size, size))
 patman_open_flipped = pygame.transform.flip(patman_open, True, False)
 
-ghost_image = pygame.image.load('ghost.png')
-ghost_image = pygame.transform.scale(ghost_image, (size, size))
+ghost_image_1 = pygame.image.load('ghost1.png')
+ghost_image_1 = pygame.transform.scale(ghost_image_1, (size, size))
+ghost_image_2 = pygame.image.load('ghost2.png')
+ghost_image_2 = pygame.transform.scale(ghost_image_2, (size, size))
+ghost_image_3 = pygame.image.load('ghost3.png')
+ghost_image_3 = pygame.transform.scale(ghost_image_3, (size, size))
+ghost_image_4 = pygame.image.load('ghost4.png')
+ghost_image_4 = pygame.transform.scale(ghost_image_4, (size, size))
 
 mouth_open = False
 flipped = False
+blocked = [False, False, False, False]
 
 scoreText = pygame.font.SysFont("monospace", 15)
 
-print(points)
+previousdirection = {0: [-1, -1], 1: [-1, -1], 2: [-1, -1], 3: [-1, -1]}
 
 while running:
     for e in pygame.event.get():
@@ -264,18 +291,68 @@ while running:
         player.move(0, -10, teleportcoords)
     if playerdirection == 3:
         flipped = False
-        player.move(0, 10, teleportcoords)  
+        player.move(0, 10, teleportcoords)
 
+    occured = []
+    ghostnum = 0
     for ghost in ghosts:
-        if ghost.direction == 0:
-            ghost.move(-10, 0, teleportcoords)
-        if ghost.direction == 1:
-            ghost.move(10, 0, teleportcoords)
-        if ghost.direction == 2:
-            ghost.move(0, -10, teleportcoords)
-        if ghost.direction == 3:
-            ghost.move(0, 10, teleportcoords)
+        blockeddirections = []
+        if ghost.rect.center in occured:
+            ghost.direction = random.randrange(0, 4)
+            if ghost.direction == 0:
+                if ghost.move(-10, 0):
+                    break
+                # print("Can't go up")
+                blockeddirections.append(ghost.direction)
+                ghost.direction = ghost.chase(player, blockeddirections)
+            if ghost.direction == 2:
+                if ghost.move(0, -10):
+                    break
+                # print("Can't go left")
+                blockeddirections.append(ghost.direction)
+                ghost.direction = ghost.chase(player, blockeddirections)
+            if ghost.direction == 3:
+                if ghost.move(0, 10):
+                    break
+                # print("Can't go right")
+                blockeddirections.append(ghost.direction)
+                ghost.direction = ghost.chase(player, blockeddirections)
+            if ghost.direction == 1:
+                if ghost.move(10, 0):
+                    break
+                # print("Can't go down")
+                blockeddirections.append(ghost.direction)
+                ghost.direction = ghost.chase(player, blockeddirections)
+        occured.append(ghost.rect.center)
 
+        ghost.direction = ghost.chase(player, blockeddirections)
+        while True:
+            if ghost.direction == 0:
+                if ghost.move(-10, 0):
+                    break
+                # print("Can't go up")
+                blockeddirections.append(str(ghost.direction))
+                ghost.direction = ghost.chase(player, blockeddirections)
+                # print(ghost.direction)
+            if ghost.direction == 1:
+                if ghost.move(10, 0):
+                    break
+                # print("Can't go down")
+                blockeddirections.append(str(ghost.direction))
+                ghost.direction = ghost.chase(player, blockeddirections)
+            if ghost.direction == 2:
+                if ghost.move(0, -10):
+                    break
+                # print("Can't go left")
+                blockeddirections.append(str(ghost.direction))
+                ghost.direction = ghost.chase(player, blockeddirections)
+            if ghost.direction == 3:
+                if ghost.move(0, 10):
+                    break
+                # print("Can't go right")
+                blockeddirections.append(str(ghost.direction))
+                ghost.direction = ghost.chase(player, blockeddirections)
+        ghostnum += 1
     running = game.check(player, ghosts)
 
     screen.fill((0,0,0))
@@ -285,19 +362,24 @@ while running:
     i = 0
 
     for point in points:
-        #print('Point:', point)
-        #print('Player:', player.rect.center)
         if player.rect.x == point[0]-10 and player.rect.y == point[1]-10:
-            print(point)
             points.pop(i)
             game.points += 1
-            print(game.points)
         else:
             pygame.draw.circle(screen, (255, 255, 255), point, 2)
         i += 1
 
+    i = 1
     for ghost in ghosts:
-        screen.blit(ghost_image, ghost.rect)
+        if i == 1:
+            screen.blit(ghost_image_1, ghost.rect)
+        if i == 2:
+            screen.blit(ghost_image_2, ghost.rect)
+        if i == 3:
+            screen.blit(ghost_image_3, ghost.rect)
+        if i == 4:
+            screen.blit(ghost_image_4, ghost.rect)
+        i += 1
 
     labelScore = scoreText.render("Score: " + str(game.points), 1, (0,0,0))
     screen.blit(labelScore, (0, -4))
@@ -317,3 +399,4 @@ while running:
     pygame.display.flip()
 
     time.sleep(0.075)
+exec(open("PATMAN.py").read())
